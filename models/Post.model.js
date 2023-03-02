5c772981a6b31868536e4ccab5cb57b242aad3ab
Diana Pena <dianaligiapena@gmail.com>
2023-03-02 17:51:54 +0200

    forum 2nd version

--- models/Post.model.js
+++ models/Post.model.js
@@ -1,28 +1,28 @@
 const { Schema, model } = require("mongoose");
 
 const postSchema = new Schema(
-  {
-    
-  title: { 
-    type: String, 
-    required: true 
-},
+    {
 
-  content: { 
-    type: String, 
-    required: true 
-},
+    title: { 
+        type: String, 
+        required: true 
+    },
 
-  author: { 
-    type: String, 
-    required: true 
-},
+    content: { 
+        type: String, 
+        required: true 
+    },
 
-  },
-  {
-    // this second object adds extra properties: `createdAt` and `updatedAt`    
-    timestamps: true
-  }
+    author: { 
+        type: Schema.Types.ObjectId, ref: 'User',
+        required: true
+    },
+
+    },
+    {
+        // this second object adds extra properties: `createdAt` and `updatedAt`    
+        timestamps: true
+    }
 );
 
 const Post = model("Post", postSchema);
